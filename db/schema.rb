bcb4f0abb0898b71aeefe584489694c8a32622e9
ADENA EISENBERG <ADENAEISENBERG@gmail.com>
2023-09-05 15:48:16 -0400

    dropped old table, made new table called workouts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_05_193911) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_05_194717) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,19 +23,18 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_05_193911) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "routines", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "exercise_id"
-    t.integer "reps"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.string "password_digest"
     t.string "image_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "workouts", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
