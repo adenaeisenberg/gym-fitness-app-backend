3d5b45dcf6f8385af796b0db9b28aff7c3df4f39
ADENA EISENBERG <ADENAEISENBERG@gmail.com>
2023-09-05 13:54:07 -0400

    added methods to routine controller and added nec routes

--- app/controllers/routines_controller.rb
+++ app/controllers/routines_controller.rb
@@ -1,2 +1,14 @@
 class RoutinesController < ApplicationController
+  def index
+    @routines = Routine.all
+    render json: @routines
+  end
+
+  def create
+    @routine = Routine.create(
+      user_id: params[:user_id],
+      exercise_id: params[:exercise_id],
+      reps: params[:reps],
+    )
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   get "exercises" => "exercises#index"
   get "exercises" => "exercises#show"
+
+  get "routines" => "routines#index"
+  post "routines" => "routines#create"
 end
