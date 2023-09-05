9353aa54ba95bf817471eca148e897a1c6d98a0b
ADENA EISENBERG <ADENAEISENBERG@gmail.com>
2023-09-05 14:03:09 -0400

    method for user create

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get "exercises" => "exercises#index"
-  get "exercises" => "exercises#show"
+  get "/exercises" => "exercises#index"
+  get "/exercises" => "exercises#show"
 
-  get "routines" => "routines#index"
-  post "routines" => "routines#create"
+  get "/routines" => "routines#index"
+  post "/routines" => "routines#create"
+
+  post "/users" => "users#create"
 end
