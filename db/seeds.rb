b87962ebbad29dcee2c6c48320ad88c1638c87c5
ADENA EISENBERG <ADENAEISENBERG@gmail.com>
2023-09-05 13:58:43 -0400

    created user seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# users = User.create([
+#   { name: "Adena", email: "adena@example.com", password_digest: "password", image_url: "https://emojis.wiki/thumbs/emojis/smiling-face-with-smiling-eyes.webp" },
+#   { name: "Eli", email: "eli@example.com", password_digest: "password", image_url: "https://emojiisland.com/cdn/shop/products/Smiling_Face_Emoji_large.png?v=1571606036" },
+# ])
