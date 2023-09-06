6be7d980be6a42a55ba347842c71d46de67e2539
ADENA EISENBERG <ADENAEISENBERG@gmail.com>
2023-09-06 12:50:17 -0400

    added column user_id to routine table

---
+++ db/migrate/20230906164921_add_column_user_id_to_routine.rb
@@ -0,0 +1,5 @@
+class AddColumnUserIdToRoutine < ActiveRecord::Migration[7.0]
+  def change
+    add_column :routines, :user_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_05_203211) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_06_164921) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,6 +30,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_05_203211) do
     t.integer "weight"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
